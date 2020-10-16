8c67d16fc759d0ab151cf331845ba54fd1024a00
Carlos Gomez <carlosg@titamedia.com>
2020-10-16 06:47:24 -0500

    add initial project

---
+++ assets/js/router.js
@@ -0,0 +1,18 @@
+/** @format */
+
+// window.onload = () => {
+// 	let sections = document.querySelectorAll('section');
+// 	sections[0].style.display = 'block'; //incicio
+// };
+
+// const inicio = (id) => {
+// 	let sections = document.querySelectorAll('section');
+// 	sections[0].style.display = 'none'; //pagina 1
+// 	sections[1].style.display = 'none'; //pagina 2
+// 	sections[2].style.display = 'none'; //pagina 3
+
+// 	if (document.getElementById) {
+// 		let etiqueta = document.getElementById(id);
+// 		etiqueta.style.display = etiqueta.style.display == 'none' ? 'block' : 'none';
+// 	}
+// };
